dcd43723e4ef256dd86edfea056b69df148c6cec
Manuel Rubio <manuel.rubio.arias@gmail.com>
2022-05-25 12:39:40 +0200

    push

---
+++ pushswap/operations_push.c
@@ -0,0 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   operations_a.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrubio <mrubio@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/30 12:35:06 by mrubio            #+#    #+#             */
+/*   Updated: 2022/05/25 11:08:19 by mrubio           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pushswap.h"
+
+void MoveDown(t_stack *stack)
+{
+	int x;
+	int size;
+
+	x = 0;
+	size = stack->size;
+	stack->size += 1;
+	stack = realloc(stack, sizeof(t_stack));
+	stack->array = realloc(stack->array, (stack->size) * sizeof(int));
+	while(size > 0)
+	{
+		stack->array[size] = stack->array[size - 1];
+		size -= 1;
+	}
+	stack->array[0] = 0;
+}
+
+void MoveUp(t_stack *stack)
+{
+	int x;
+	int size;
+
+	x = 0;
+	size = stack->size;
+	stack->size -= 1;
+	stack = realloc(stack, sizeof(t_stack));
+	stack->array = realloc(stack->array, (stack->size) * sizeof(int));
+	while(size > 0)
+	{
+		stack->array[x] = stack->array[x + 1];
+		size -= 1;
+		x++;
+	}
+}
+
+void	pb(t_stack *a, t_stack *b)
+{
+	if (a->size == 0)
+		return;
+	MoveDown(b);
+	b->array[0] = a->array[0];
+	MoveUp(a);
+	write(1, "pb\n", 3);
+}
+
+void	pa(t_stack *a, t_stack *b)
+{
+	if (b->size == 0)
+		return;
+	MoveDown(a);
+	a->array[0] = b->array[0];
+	MoveUp(b);
+	write(1, "pa\n", 3);
+}
--- pushswap/pushswap.c
+++ pushswap/pushswap.c
@@ -6,7 +6,7 @@
 /*   By: mrubio <mrubio@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/30 09:37:27 by mrubio            #+#    #+#             */
-/*   Updated: 2022/05/23 10:21:42 by mrubio           ###   ########.fr       */
+/*   Updated: 2022/05/25 11:07:31 by mrubio           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,15 @@
 
 void	pushswap(t_stack *a, t_stack *b)
 {
+	pa(a, b);
+	pb(a, b);
+	pb(a, b);
+	pb(a, b);
+	pb(a, b);
+	pa(a, b);
+	pa(a, b);
+	pb(a, b);
+	pa(a, b);
+	pa(a, b);
 	print_stacks(a, b);
 }
